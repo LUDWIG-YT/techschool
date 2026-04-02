8afb5f979593a1822d1fd651a0d1079541859a70
LUDWIG-YT <foxy51anime2@gmail.com>
2026-04-02 16:28:57 -0600

    Delete js directory

--- js/dashboard.js
+++
@@ -1,9 +0,0 @@
-if(!localStorage.getItem("auth")){
-window.location="login.html";
-}
-
-document.getElementById("cmd").addEventListener("keydown",e=>{
-if(e.key==="Enter"){
-alert("Comando ejecutado");
-}
-});
